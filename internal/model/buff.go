afac8f041081baa2a3ecd950c631fbb7c829bb20
J.C.Reid <jcr32@kent.ac.uk>
2020-07-01 09:34:20 +0100

    document internal/model package

--- internal/model/buff.go
+++ internal/model/buff.go
@@ -2,6 +2,14 @@ package model
 
 import "github.com/google/uuid"
 
+// BuffStore defines all the actions needed to implement a buff storage layer
+// This could be implemented by:
+//   - A relational database (for production)
+//   - A mock implementation (for testing)
+//   - An RPC backend (for unforeseen future developments)
+//
+// Genericising the storage actions in this way makes the code considerably
+// easier to re-factor with respect to storage sub-systems, should they need to change
 type BuffStore interface {
 	GetBuff(BuffID) (*Buff, error)
 	ListBuff(offset, limit int) ([]Buff, error)
@@ -12,8 +20,27 @@ type BuffStore interface {
 	DeleteBuff(BuffID) error
 }
 
+// BuffID is a uuid.UUID type
+// It is defined as it's own type to make the use of IDs in the model type-safe
+// E.g. you can't accidentally use a BuffID as a VideoStreamID
 type BuffID uuid.UUID
 
+// String provides access to the underlying uuid.String function
+func (b BuffID) String() string {
+	return uuid.UUID(b).String()
+}
+
+// ParseBuffID will return a new BuffID parsed from it's string representation
+// The string is expected in a valid uuid.UUID format
+func ParseBuffID(s string) (BuffID, error) {
+	id, err := uuid.Parse(s)
+	return BuffID(id), err
+}
+
+// Buff defines the abstract representation of the Buff type in the data model
+//
+// This is how we can think about a Buff in the application
+// (separate from the database or API encoding representations)
 type Buff struct {
 	ID       BuffID
 	Stream   VideoStreamID
@@ -21,27 +48,26 @@ type Buff struct {
 	Answers  []Answer
 }
 
-func (b BuffID) String() string {
-	return uuid.UUID(b).String()
-}
-
-func ParseBuffID(s string) (BuffID, error) {
-	id, err := uuid.Parse(s)
-	return BuffID(id), err
-}
-
+// Answer defines the abstract representation of the Answer type in the data model
+// It is not meant to be used in isolation from a Buff type
 type Answer struct {
 	ID      AnswerID
 	Text    string
 	Correct bool
 }
 
+// AnswerID is a uuid.UUID type
+// It is defined as it's own type to make the use of IDs in the model type-safe
+// E.g. you can't accidentally use a BuffID as a VideoStreamID
 type AnswerID uuid.UUID
 
+// String provides access to the underlying uuid.String function
 func (a AnswerID) String() string {
 	return uuid.UUID(a).String()
 }
 
+// ParseAnswerID will return a new AnswerID parsed from it's string representation
+// The string is expected in a valid uuid.UUID format
 func ParseAnswerID(s string) (AnswerID, error) {
 	id, err := uuid.Parse(s)
 	return AnswerID(id), err
--- internal/model/model.go
+++ internal/model/model.go
@@ -2,8 +2,18 @@ package model
 
 import "errors"
 
+// ErrNotFound should be returned by store implementations when they
+// couldn't find the requested data
 var ErrNotFound = errors.New("the requested data was not found in the store")
 
+// Store defines all the actions needed to implement the full storage layer
+// This could be implemented by:
+//   - A relational database (for production)
+//   - A mock implementation (for testing)
+//   - An RPC backend (for unforeseen future developments)
+//
+// Genericising the storage actions in this way makes the code considerably
+// easier to re-factor with respect to storage sub-systems, should they need to change
 type Store interface {
 	VideoStreamStore
 	BuffStore
