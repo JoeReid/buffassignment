46d3baf0e169d99af232c1008ba7ca0862eef6dd
J.C.Reid <jcr32@kent.ac.uk>
2020-07-01 09:09:48 +0100

    update api/videostream docs

--- api/videostream/list.go
+++ api/videostream/list.go
@@ -8,14 +8,25 @@ import (
 	"github.com/JoeReid/buffassignment/internal/model"
 )
 
+// NewListHandler returns a new instance of the list action of
+// the videostream API using the given store instance.
+//
+// The store is provided as an argument for easy dependency injection in tests
+// I.E: using the testing mock store rather than a full DB for API testing
 func NewListHandler(store model.VideoStreamStore) apiutils.Handler {
 	return &streamList{store}
 }
 
+// streamGet implements the apiutils.Handler interface to provide the
+// list portion of the videostream API
 type streamList struct {
 	store model.VideoStreamStore
 }
 
+// ServeCodec serves the API using the apiutils.Handler pattern
+// This allows the business logic to live here, and the encoding to live separate from it
+// This also makes testing easier, as there is a test codec that allows us to peek at the output
+// in a testing context.
 func (s *streamList) ServeCodec(c apiutils.Codec, w http.ResponseWriter, r *http.Request) {
 	count, skip, err := apiutils.Paginate(r, apiutils.DefaultCount(10), apiutils.MaxCount(10))
 	if err != nil {
